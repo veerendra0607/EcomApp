70ddf3b457d5e7a2b9eaa1123d298847040610c2
Apple <apple@Apples-MacBook-Pro.local>
2023-05-05 18:08:29 +0530

    home1:35

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return (<>
+  <section className='home-wrapper'>
+    <div className='container-xxl'>
+      <div className='row'>
+        <div className='col-6'>
+          <div className='main-banner position-relative p-3'>
+            <img src='images/main-banner-1.jpg' className='img-fluid rounded-3' alt='main banner'></img>
+            <div className='main-banner-content position-absolute'>
+              <h4> SUPERCHARGED FOR PROS</h4>
+              <h5>iPad S13+ Pro.</h5>
+              <p>From $999.00 or $41.62/mo.</p>
+              <Link> Buy Now</Link>
+            </div>
+          </div>
+        </div>
+        <div className='col-6'></div>
+      </div>
+    </div>
+
+  </section>
+  </>)
 }
 
 export default Home
